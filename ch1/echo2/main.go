9ea8f26bf40a8ab77400f224983ad5f6037b4597
Shubham Turai <shubham.turai@gmail.com>
2020-03-04 15:55:58 +0100

    lets check credentials

--- ch1/echo2/main.go
+++ ch1/echo2/main.go
@@ -1,9 +1,9 @@
 // Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-/////
+
 // See page 6.
 //!+
-//
+
 // Echo2 prints its command-line arguments.
 package main
 
