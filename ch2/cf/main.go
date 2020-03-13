c7c2ac5baef1715b2be54eda7c13fa8ed2d1573d
Shubham Turai <shubham.turai@gmail.com>
2020-03-13 13:58:59 +0100

    exercise 2.2 partially to conversions : stdio,args

--- ch2/cf/main.go
+++ ch2/cf/main.go
@@ -8,6 +8,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
@@ -16,17 +17,45 @@ import (
 )
 
 func main() {
-	for _, arg := range os.Args[1:] {
-		t, err := strconv.ParseFloat(arg, 64)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
-			os.Exit(1)
+	if len(os.Args[1:]) > 1{
+		for _, arg := range os.Args[1:] {
+			t, err := strconv.ParseFloat(arg, 64)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+				os.Exit(1)
+			}
+			f := tempconv.Fahrenheit(t)
+			c := tempconv.Celsius(t)
+			fmt.Printf("%s = %s, %s = %s\n",
+				f, tempconv.FToC(f), c, tempconv.CToF(c))
 		}
-		f := tempconv.Fahrenheit(t)
-		c := tempconv.Celsius(t)
-		fmt.Printf("%s = %s, %s = %s\n",
-			f, tempconv.FToC(f), c, tempconv.CToF(c))
+	} else{
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			fmt.Println("enter the value for conversion or press q to quit")
+			key, err := reader.ReadString('\n')
+			key = key[:len(key)-1]
+			if err == nil && key != "q" {
+				fmt.Println("the key is : ", key)
+				value, err := strconv.Atoi(key)
+				fmt.Println("the error found was :", err)
+				if err == nil {
+					f := tempconv.Fahrenheit(value)
+					c := tempconv.Celsius(value)
+					fmt.Printf("%s = %s, %s = %s\n\n",
+						f, tempconv.FToC(f), c, tempconv.CToF(c))
+				} else{
+					fmt.Println("could not convert the entered value to integer")
+				}
+			} else if err != nil {
+				fmt.Println("the entered value could not be read. please enter other value of press q to quit")
+			} else if key == "q"{
+				break
+			}
+			
+		}
 	}
+	
 }
 
 //!-
