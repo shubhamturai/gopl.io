a715ad7c2b6fcee32e82999bdc5feec61127dcd1
Shubham Turai <shubham.turai@gmail.com>
2020-04-10 17:10:00 +0200

    exercise 7.1 word & line counter

--- ch7/bytecounter/main.go
+++ ch7/bytecounter/main.go
@@ -8,15 +8,56 @@ package main
 
 import (
 	"fmt"
+	"bufio"
 )
 
 //!+bytecounter
-
+// ByteCounter counts the number of bytes
 type ByteCounter int
+
+// WordCounter count words
+type WordCounter int
+
+// LineCounter count Lines
+type LineCounter int
 
 func (c *ByteCounter) Write(p []byte) (int, error) {
 	*c += ByteCounter(len(p)) // convert int to ByteCounter
 	return len(p), nil
+}
+
+type scanFunc func(p []byte, EOF bool) (advance int, token []byte, err error)
+
+func scanBytes(p []byte, fn scanFunc) (cnt int) {
+    for true {
+        advance, token, _ := fn(p, true)
+        if len(token) == 0 {
+            break
+        }
+        p = p[advance:]
+        cnt++
+    }
+    return cnt
+}
+
+func (c *WordCounter) Write(p []byte) (int, error) {
+    cnt := scanBytes(p, bufio.ScanWords)
+    *c += WordCounter(cnt)
+    return cnt, nil
+}
+
+func (c WordCounter) String() string {
+    return fmt.Sprintf("contains %d words", c)
+}
+
+func (c *LineCounter) Write(p []byte) (int, error) {
+    cnt := scanBytes(p, bufio.ScanLines)
+    *c += LineCounter(cnt)
+    return cnt, nil
+}
+
+func (c LineCounter) String() string {
+    return fmt.Sprintf("contains %d lines", c)
 }
 
 //!-bytecounter
@@ -32,4 +73,28 @@ func main() {
 	fmt.Fprintf(&c, "hello, %s", name)
 	fmt.Println(c) // "12", = len("hello, Dolly")
 	//!-main
+	var w WordCounter
+	w.Write([]byte("hello world"))
+    fmt.Println(w)
+
+    fmt.Fprintf(&w, "This is an sentence.")
+    fmt.Println(w)
+
+    w = 0
+    fmt.Fprintf(&w, "This")
+    fmt.Println(w)
+
+	var l LineCounter
+	l.Write([]byte("hello world\nwelcome"))
+    fmt.Println(l)
+
+    fmt.Fprintf(&l, `This is another line`)
+    fmt.Println(l)
+
+    l = 0
+    fmt.Fprintf(&l, "This is another\nline")
+    fmt.Println(l)
+
+    fmt.Fprintf(&l, "This is one line")
+    fmt.Println(l)
 }
